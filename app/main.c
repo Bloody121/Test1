678a71381b5abcf6e683809c113e63e6a8744ac2
Bloody121 <lexa.2tri@yandex.ru>
2019-05-22 15:46:05 +0300

    Update main.c

--- app/main.c
+++ app/main.c
@@ -13,9 +13,10 @@ int main()
     if((scanf("%lf %lf %lf", &a, &b, &c)) != 0 ){
 
     val = Func(a, b, c);
-                                                }
-    if(val.count==2){
+                                                
+    if(val.count==2)
         printf("%.2lf, %.2lf\n", val.x1, val.x2);
+    
     }
     return 0;
 }
