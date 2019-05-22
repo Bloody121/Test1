cb6b470e2b7411cfd4b336edb5736908cefb428c
Bloody121 <lexa.2tri@yandex.ru>
2019-05-22 14:48:56 +0300

    Update main.c

--- app/main.c
+++ app/main.c
@@ -5,15 +5,17 @@
 int main()
 {
     values val;
-    double a, b, c;
+    double a;
+    double b;
+    double c;
 
     printf("Введите коэффиценты a, b, c:\n");
-    if((scanf("%lf %lf %lf", &a, &b, &c)) != 0 ){;
+    if((scanf("%lf %lf %lf", &a, &b, &c)) != 0 ){
 
     val = Func(a, b, c);
                                                 }
-    if(val.count==2)
+    if(val.count==2){
         printf("%.2lf, %.2lf\n", val.x1, val.x2);
-
+    }
     return 0;
 }
